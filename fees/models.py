fc52579304862d4d3499bb51dc62b8c366d631a6
Sandy <codingsandy6@gmail.com>
2026-01-13 00:10:00 +0545

    Feat: Created the basic models of all apps

--- fees/models.py
+++ fees/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from students.models import Student
 
-# Create your models here.
+class Fee(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    due_date = models.DateField()
+    paid = models.BooleanField(default=False)
+
--- students/models.py
+++ students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    grade = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    guardian_name = models.CharField(max_length=100)
+    guardian_phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.full_name
