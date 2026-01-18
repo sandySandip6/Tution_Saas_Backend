afa9e472605265eebb8c868241cda5e597e501cd
Sandy <codingsandy6@gmail.com>
2026-01-18 13:12:33 +0545

    Fix: removed admin.py code in attendance, fee and students model

--- fees/admin.py
+++ fees/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Fee
-=======
-from .models import Fee, FeeCategory
->>>>>>> 2bc03f151455db2cd1293f71eb0da35d96c394f6
 
-@admin.register(Fee)
-class FeeAdmin(admin.ModelAdmin):  
-    list_display = ('id', 'amount', 'due_date', 'category')
-    search_fields = ('category__name',)
-    list_filter = ('due_date',)
--- students/admin.py
+++ students/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Student
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):  
-    list_display = ('id', 'first_name', 'last_name', 'email')
-    search_fields = ('first_name', 'last_name', 'email')
-    list_filter = ('enrollment_date',)
